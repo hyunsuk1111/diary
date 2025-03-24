a5ec6e7af17b03917ba466d831de1de7b4ce3022
hyunsuk1111 <epsxkf@gmail.com>
2025-03-24 16:50:19 +0900

    FEAT : 뷰 연동
    스프링부트 뷰연동

--- src/main/java/com/example/diary/config/SecurityConfig.java
+++ src/main/java/com/example/diary/config/SecurityConfig.java
@@ -9,6 +9,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록이 됨.
@@ -19,7 +22,7 @@ public class SecurityConfig {
         return http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests((authorizeRequests) -> {
-                    authorizeRequests.requestMatchers("/auth/login", "/auth/register").permitAll(); // 로그인과 회원가입은 인증 없이 접근 가능
+                    authorizeRequests.requestMatchers("/auth/login", "/auth/register","/error","/", "/index.html", "/css/**", "/js/**").permitAll(); // 로그인과 회원가입은 인증 없이 접근 가능
                     authorizeRequests.anyRequest().authenticated(); // 그 외 요청은 인증 필요
                 })
                 .addFilterBefore(new JwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
@@ -29,5 +32,18 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("http://localhost:8081");
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
