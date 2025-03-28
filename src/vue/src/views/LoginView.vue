f247bbf5356eeab4ff18e731619104d75fab0923
hyunsuk1111 <epsxkf@gmail.com>
2025-03-28 22:17:37 +0900

    FEAT : 로그인 vue 진행중
    로그인 vue 진행중

--- src/vue/src/views/LoginView.vue
+++ src/vue/src/views/LoginView.vue
@@ -1,11 +1,32 @@
 <template>
-  <div id="app">
-    <h1>LoginView</h1>
+  <div id="login-wrapper">
+    <form @submit.prevent="handleLogin">
+      <input type="text" placeholder="아이디" v-model="email">
+      <input type="password" placeholder="비밀번호" v-model="password">
+      <button>로그인</button>
+    </form>
   </div>
 </template>
 
 <script setup>
-  // Composition API 사용 가능 (JS 버전)
+  import axios from 'axios';
+  import { ref } from 'vue'
+
+  const email = ref('');
+  const password = ref('');
+
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post('/auth/login', {
+        email: email.value,
+        password: password.value
+      })
+      
+      console.log('로그인 성공 ', response.data);
+    } catch (error) {
+      console.error('로그인 실패', error);
+    }
+  }//handleLogin
 </script>
 
 <style>
