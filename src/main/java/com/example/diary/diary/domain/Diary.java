71253dabb635750ceb7a1c463f2e4d30f3594912
hyunsuk1111 <epsxkf@gmail.com>
2025-04-01 22:14:34 +0900

    MOD : 일기장 도메인 변경
    일자별 일기 구분을 위한 diaryDate추가

--- src/main/java/com/example/diary/diary/domain/Diary.java
+++ src/main/java/com/example/diary/diary/domain/Diary.java
@@ -2,8 +2,11 @@ package com.example.diary.diary.domain;
 
 import com.example.diary.common.BaseEntity;
 import com.example.diary.user.domain.User;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.time.LocalDate;
 
 @Entity
 @Getter
@@ -12,10 +15,11 @@ import lombok.*;
 public class Diary extends BaseEntity {
 
     @Builder
-    public Diary(Long id, String title, String content, User user) {
+    public Diary(Long id, String title, String content,LocalDate diaryDate, User user) {
         this.id = id;
         this.title = title;
         this.content = content;
+        this.diaryDate = diaryDate;
         this.user = user;
     }
 
@@ -29,6 +33,10 @@ public class Diary extends BaseEntity {
 
     @Column(nullable = false)
     private String content;
+
+    @Column(nullable = false)
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate diaryDate;
 
     @ManyToOne
     @JoinColumn(name = "user_id")
